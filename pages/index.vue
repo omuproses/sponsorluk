8e30dcb42df86901327b461f73aa216e734fa549
omuproses <bugrahan-isler55@hotmail.com>
2024-10-28 01:16:08 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -54,27 +54,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import { cafes } from '~/cafes.js'; // Import cafes data
 
-const cafes = ref([
-{
-	name: "Cafe 1",
-	yazi: "Cafe 1 kafesinde %10 indirim",
-	image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
-},
-{
-	name: "Cafe 2",
-	yazi: "Cafe 2 kafesinde %15 indirim",
-	image: "https://i.pinimg.com/564x/98/ff/1f/98ff1f34046a7e1a0f112a5a1856050e.jpg",
-},
-{
-	name: "Cafe 3",
-	yazi: "Cafe 3 kafesinde %20 indirim",
-	image: "/cafe.jpg",
-},
-{
-	name: "Cafe 4",
-	yazi: "Cafe 4 kafesinde %25 indirim",
-	image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
-},
-]);
+const cafesRef = ref(cafes);
 </script>
