2506a737989e6f6a6d915c6b67975efe6a60bb4f
omuproses <bugrahan-isler55@hotmail.com>
2024-10-28 01:15:17 +0300

    Add files via upload

---
+++ cafes.js
@@ -0,0 +1,26 @@
+// /data/cafesData.js
+import cafeImage from '~/assets/img/cafe.jpg';
+
+export const cafes = [
+    {
+      name: "Cafe 1",
+      image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
+      discount: 10,
+    },
+    {
+      name: "Cafe 2",
+      image: "https://i.pinimg.com/564x/98/ff/1f/98ff1f34046a7e1a0f112a5a1856050e.jpg",
+      discount: 15,
+    },
+    {
+      name: "Cafe 3",
+      image: cafeImage,
+      discount: 20,
+    },
+    {
+      name: "Cafe 4",
+      image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
+      discount: 20,
+    },
+  ];
+  
