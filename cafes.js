147a6dfa2b3b5979d31cffabe033edae8256d11f
omuproses <bugrahan-isler55@hotmail.com>
2024-10-28 01:19:55 +0300

    Update cafes.js

--- cafes.js
+++ cafes.js
@@ -3,24 +3,24 @@ import cafeImage from '~/assets/img/cafe.jpg';
 
 export const cafes = [
     {
-      name: "Cafe 1",
-      image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
-      discount: 10,
+        name: "Cafe 1",
+	    yazi: "Cafe 1 kafesinde %10 indirim",
+	    image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
     },
     {
-      name: "Cafe 2",
-      image: "https://i.pinimg.com/564x/98/ff/1f/98ff1f34046a7e1a0f112a5a1856050e.jpg",
-      discount: 15,
+        name: "Cafe 2",
+	    yazi: "Cafe 2 kafesinde %15 indirim",
+	    image: "https://i.pinimg.com/564x/98/ff/1f/98ff1f34046a7e1a0f112a5a1856050e.jpg",
     },
     {
-      name: "Cafe 3",
-      image: cafeImage,
-      discount: 20,
+      	name: "Cafe 3",
+	    yazi: "Cafe 3 kafesinde %20 indirim",
+	    image: "/cafe.jpg",
     },
     {
-      name: "Cafe 4",
-      image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
-      discount: 20,
+        name: "Cafe 4",
+	    yazi: "Cafe 4 kafesinde %25 indirim",
+	    image: "https://www.yapikatalogu.com/Files/Project/571/file-3216.jpg",
     },
   ];
   
